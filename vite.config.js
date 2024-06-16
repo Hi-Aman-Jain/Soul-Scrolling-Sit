7d86206832a8a7794696add302e7875a3aeed468
Hi-Aman-Jain <hi.jainaman@gmail.com>
2024-06-16 21:48:04 +0530

    Three configured

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Soul_Scrolling_Site', 
+  base: '/Soul-Scrolling-Site/',
+  optimizeDeps: {
+    include: ['three'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['three'],
+      output: {
+        globals: {
+          three: 'THREE',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
